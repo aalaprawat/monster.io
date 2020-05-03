6ff5291931effc7a17eb26b7d2a014f0e72c3d55
aalaprawat <aalaprawatdurg@gmail.com>
2020-05-03 22:34:12 +0530

    Updates

---
+++ precache-manifest.7ea7dbd668cba59f0a34972a333aeb9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021a1f884df381eeb33a65bcb96f9105",
+    "url": "/monster.io/index.html"
+  },
+  {
+    "revision": "212639bf55e285e25a84",
+    "url": "/monster.io/static/css/main.e91cac8c.chunk.css"
+  },
+  {
+    "revision": "5eed07350c569f900072",
+    "url": "/monster.io/static/js/2.38ca39a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster.io/static/js/2.38ca39a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212639bf55e285e25a84",
+    "url": "/monster.io/static/js/main.cc2c68de.chunk.js"
+  },
+  {
+    "revision": "44ee0d30828673003e75",
+    "url": "/monster.io/static/js/runtime-main.213c96c7.js"
+  }
+]);
